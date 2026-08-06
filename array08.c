ebbc441ffbbc4a213167ea0e43a1f737184ceae4
Md. Yeanath Hossain <yeanathhossain12345@gmail.com>
2026-08-07 02:51:58 +0600

    Refactor sorting functions and improve item display format

--- array08.c
+++ array08.c
@@ -1,42 +1,61 @@
-// Descending order
+// ascending and descending order
 
 #include <stdio.h>
-
 #define n 6
 
-void showItems(int a[], int sz)
+void show_items(int list[], int sz)
 {
     for (int i = 0; i < sz; i++)
-        printf("%d ", a[i]);
-    printf("\n");
-}
-
-int main(void)
-{
-    int ar[n] = {6, 3, 1, 9, 7, 2};
-
-    showItems(ar, n);
-
-    int m, t;
-    for (int i = 0; i < n; i++)
     {
-        m = i;
-        for (int j = i + 1; j < n; j++)
+        printf("%4d", list[i]);
+    }
+    printf("\n");
+    return;
+}
+void ascending_order(int list[], int sz)
+{
+    int t;
+    for (int i = 0; i < sz; i++)
+    {
+        for (int j = i + 1; j < sz; j++)
         {
-            if (ar[j] > ar[m])
-                m = j;
-        }
-
-        if (m != i)
-        {
-            t = ar[i];
-            ar[i] = ar[m];
-            ar[m] = t;
+            if (list[i] > list[j])
+            {
+                t = list[i];
+                list[i] = list[j];
+                list[j] = t;
+            }
         }
     }
+    return;
+}
 
-    showItems(ar, n);
+void descending_order(int list[], int sz)
+{
+    int t;
+    for (int i = 0; i < sz; i++)
+    {
+        for (int j = i + 1; j < sz; j++)
+        {
+            if (list[i] < list[j])
+            {
+                t = list[i];
+                list[i] = list[j];
+                list[j] = t;
+            }
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int list[n] = {4, 6, 5, 8, 7, 2};
+    show_items(list, n);
+    ascending_order(list, n);
+    show_items(list, n);
+    descending_order(list, n);
+    show_items(list, n);
 
     return 0;
 }
-
